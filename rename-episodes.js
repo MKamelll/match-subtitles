3cbd54d33451b56c4fe46ed7e2692733462085c4
George <minallkamel@gmail.com>
2019-06-25 02:58:29 +0200

    fixed a duplication bug"

--- rename-episodes.js
+++ rename-episodes.js
@@ -2,7 +2,7 @@
   script to iterate through media files and change the subs files
   to be the same as the media files
 */
- 
+
 // dependencies
 const fs = require('fs');
 const chalk = require('chalk');
@@ -10,11 +10,11 @@ const extensions = require('./extens.json');
 
 // vars
 let path = process.cwd(),
-mediaSelector, 
-subSelector;
+  mediaSelector,
+  subSelector;
 
 // init
-(function init (path) {
+(function init(path) {
   console.log(chalk.yellow(`${`Folder: ${path}`}`));
   return dirList(path);
 })(path);
@@ -23,19 +23,18 @@ subSelector;
 function dirList(path) {
   fs.readdir(path, (err, files) => {
     if (err) throw err;
-    files.some((file) => {
+    files.some(file => {
       if (file.match(/(\.\w+\d?)$/gi)) {
         const ext = file.match(/(\.\w+\d?)$/gi)[0];
         if (extensions['media'][ext]) {
           mediaSelector = ext;
         } else if (extensions['subs'][ext]) {
           subSelector = ext;
-        }  
+        }
       }
     });
     return filterFiles(files, mediaSelector, subSelector);
   });
-
 }
 
 // filter only files that is any mediaSelector as provided
@@ -44,13 +43,13 @@ function filterFiles(files, mediaSelector, subSelector) {
   const mediaFiltered = [];
   const subFiltered = [];
 
-  files.forEach((file) => {
+  files.forEach(file => {
     if (file.includes(`${mediaSelector}`)) {
       mediaFiltered.push(file.replace(`${mediaSelector}`, `${subSelector}`));
     } else if (file.includes(`${subSelector}`)) {
       subFiltered.push(file);
     }
-  });  
+  });
   return matchFiles(mediaFiltered, subFiltered);
 }
 
@@ -68,7 +67,7 @@ function matchFiles(mediaFiles, subFiles) {
     let number = extractEpisodesNumber(currentName);
     subFiles[currentName] = mediaNames[number];
     return subFiles;
-  }, {});  
+  }, {});
   return renameAllFiles(renameFiles);
 }
 
@@ -76,7 +75,7 @@ function matchFiles(mediaFiles, subFiles) {
 function extractEpisodesNumber(currentName) {
   let number;
   if (/\[?s?\d+?e(\d+)/gi.test(currentName)) {
-    number = parseInt(/\[?s?\d+?e(\d+)/gi.exec(currentName)[1]);      
+    number = parseInt(/\[?s?\d+?e(\d+)/gi.exec(currentName)[1]);
   } else if (/\[?\d+x?(\d+)/g.test(currentName)) {
     number = parseInt(/\[?\d+x?(\d+)/g.exec(currentName)[1]);
   } else {
@@ -85,26 +84,45 @@ function extractEpisodesNumber(currentName) {
   return number;
 }
 
-// rename each file as the old name is an object key 
+// rename each file as the old name is an object key
 // and the new name is its value
 function renameAllFiles(filesObject) {
   let counter = 0;
-  Object.keys(filesObject).forEach((subName) => {    
-    if (filesObject[subName] && subName) {      
-      fs.rename(`${path}\\${subName}`, `${path}\\${filesObject[subName]}`, (err, _) => {
-        if (err) throw err;
-        counter += 1;
-        console.log(`${chalk.yellow('Renamed:')} ${chalk.green(`${filesObject[subName]}`)}`);
-        
-        if (counter === Object.keys(filesObject).length) {
-          console.log(`${chalk.yellow(counter)} ${chalk.blue(`Files Renamed!`)}`)
+  let renamed = 0;
+  Object.keys(filesObject).forEach(subName => {
+    if (filesObject[subName] && subName) {
+      fs.rename(
+        `${path}\\${subName}`,
+        `${path}\\${filesObject[subName]}`,
+        (err, _) => {
+          if (err) {
+            // check for dublication errors
+            if (err.code === 'EPERM' && err.syscall === 'rename') {
+              console.error('Oops, dublicate subtitles!');
+            } else {
+              throw err;
+            }
+          } else {
+            // count only renamed ones
+            renamed += 1;
+          }
+          // count all files
+          counter += 1;
+          console.log(
+            `${chalk.yellow('Renamed:')} ${chalk.green(
+              `${filesObject[subName]}`
+            )}`
+          );
+
+          if (counter === Object.keys(filesObject).length) {
+            console.log(
+              `${chalk.yellow(renamed)} of ${chalk.green(counter)} ${chalk.blue(
+                `Files Renamed!`
+              )}`
+            );
+          }
         }
-      });
+      );
     }
   });
 }
-
-
-
-      
-
